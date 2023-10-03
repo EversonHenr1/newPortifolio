7285a6b9f59889ef797f469db3f7687d2a4e9e03
Everson Henrique <117359517+EversonHenr1@users.noreply.github.com>
2023-10-03 20:31:36 -0300

    Atualizacao_03_10

--- newPortifolio/javaScript/index.js
+++ newPortifolio/javaScript/index.js
@@ -6,8 +6,8 @@
     //Array com graficos
     let grafHabilidade = []
         //Arrays com informações dos graficos
-        let habilidades = ["Excel","HTML","css","JavaScript","SQL","linguagem C","Linguagem UML","Java(android)","Node.JS"]
-        let habilidadeTam =["40%","70%","68%","35%","40%","35%","25%","20%","10%"]
+        let habilidades = ["Excel","HTML","css","JavaScript","SQL","linguagem C","Linguagem UML","Java (android)","Node.JS","React"]
+        let habilidadeTam =[40,70,68,35,40,10,25,20,25,15]
 
             //ForEach que criara os graficos
             habilidades.forEach((e,i,a)=>{
@@ -21,7 +21,43 @@
                 //Aplicar  as informações dos graficos
                 hab.textContent = e;
                 porcentagem.textContent ="("+ habilidadeTam[i] + ")"
-                progress.style.width = habilidadeTam[i]; 
+                    
+                window.addEventListener("scroll",()=>{
+                    const containerGraficos = document.querySelector("#habilidades")
+                    
+                    const graficoOffset = containerGraficos.offsetTop;
+                    
+                    const scrollPosition = window.scrollY || window.pageYOffset
+                    
+                    let opacity  = 1 +(scrollPosition - graficoOffset) / 900;
+
+                    opacity = Math.min(1,Math.max(0,opacity));
+
+                    if(opacity >= 1){
+
+                        progress.style.width = `${habilidadeTam[i]}%`; 
+                        let cont = 0
+                        const attNumber = setInterval(()=>{
+                            
+                            porcentagem.innerHTML = `${cont}%`
+                            cont ++
+                            if(cont > habilidadeTam[i]){
+                                clearInterval(attNumber)
+                            }
+                        })
+                       
+                    }else{
+                        progress.style.width = "0px"
+                    }
+
+                    containerGraficos.style.opacity = opacity
+                    console.log(opacity)
+             
+                })
+                    
+
+                //Tamanho
+                
                 //Aplicar o conteudo ao Site
                 paiHabilidade.appendChild(template)
             })
@@ -93,11 +129,15 @@
         }
         //Inserir objetos com todas as informações
         const ex0 = new ex("Estagiario ou Junior TI","coming soon","Estou em busca de uma oportunidade na qual possa adquirir novas experiências, como ingressante na área de TI, na qual curso faculade e conheço algumas linguagens como apresentado abaixo;","","",0,0,"none")
-        const ex1 = new ex("Jovem aprendiz administrativo","21/02/2022 a 13/01/2023", "Dentro da empresa minhas principais atividades são: Realizar questionamento com motorista após realizarem suas viajens, Realizar a baixa de finalização de entregas, Realizar leituras de coletas e baixa-las no sistema, Fazer questionamentos e lançar ocorrencias no sistema, Arquivamento de entregas.","https://www.viapajucara.com.br/","https://www.linkedin.com/company/viapajucaratransportes/",1,1,"inline")
-        const ex2 = new ex("Estagio Auxiliar Geral (contagem)","13/04/2021 a 31/12/2021","Dentro da empresa tinha como principais atividades: Realizar a contagem dos produtos no armazem e no espaço de vendas, Fazer lançamento dos resultados adquiros no sistema  Comunicão com o setor de inventario; Além  de ajudar no abastecimento, e atendimento ao cliente.","https://xsupermercados.com.br/","https://www.linkedin.com/company/x-supermercados/",1,1,"inline")
+        const ex2 = new ex("Jovem aprendiz administrativo","21/02/2022 a 13/01/2023", "Dentro da empresa minhas principais atividades são: Realizar questionamento com motorista após realizarem suas viajens, Realizar a baixa de finalização de entregas, Realizar leituras de coletas e baixa-las no sistema, Fazer questionamentos e lançar ocorrencias no sistema, Arquivamento de entregas.","https://www.viapajucara.com.br/","https://www.linkedin.com/company/viapajucaratransportes/",1,1,"inline")
+        const ex3 = new ex("Estagio Auxiliar Geral (contagem)","13/04/2021 a 31/12/2021","Dentro da empresa tinha como principais atividades: Realizar a contagem dos produtos no armazem e no espaço de vendas, Fazer lançamento dos resultados adquiros no sistema  Comunicão com o setor de inventario; Além  de ajudar no abastecimento, e atendimento ao cliente.","https://xsupermercados.com.br/","https://www.linkedin.com/company/x-supermercados/",1,1,"inline")
+        const ex1 = new ex("Estagio Logistica","18/09/2023","Atualmente trabalho no setor de Logistica da Nucleo Ferroligas, tendo com função agendar coletas, preencher e editar planilhas , e atividades Administrativas além de ajudar em algumas coisas na parte de Tecnologia","https://nucleoligas.com.br/","https://www.linkedin.com/company/nucleoligas",1,1,"inline")
+        
+        
         //Criar a arr com todos os objetos
         const experi = []
         experi.push(ex0); experi.push(ex1); experi.push(ex2);
+        experi.push(ex3);
 
             btnEx.forEach((e,i,a)=>{
                 e.addEventListener("click",()=>{
@@ -183,9 +223,9 @@
          const proj1 = new proj("./imagens/projetos/zoo.png","zoologico de guarulhos","Projeto acâdemico (primeiro site), reformulação site site Zoologico de Guarulhos","","https://eversonhenr1.github.io/site_Zoologico_Guarulhos/Zoologico/",null,null,[html,css])
         const proj2 = new proj("./imagens/projetos/cpfitness.png","Academia CP Fitness","Projeto acâdemico (primeiro site com banco de dados), desenvolver um site que tenha Banco da dados SQL","","https://eversonhenr1.github.io/site_Cp_Fitness/Home/","Site = Cancel\n\nVideo = OK","https://www.youtube.com/watch?v=0IAMcx7rZpA",[html,css,javascript,sql])
         const proj3 = new proj("./imagens/projetos/zologico.png","Zoologico reformulado","Em processo de Desenvolvimento. Reformulação do meu primeiro site com novos conhecimentos adquiridos","fa-lock","","Site em Desenvolvimento",null,[html,css,javascript])
-        const proj4 = new proj("./imagens/projetos/farn2.png","Farm","Projeto em desenvolvimento, com o objetivo de criar um  aplicativo em react para compartilhar de receitas","fa-youtube-play","https://youtu.be/NV8wRY7XmN0",null,null,[react,html,css,javascript])
+        const proj4 = new proj("./imagens/projetos/farn2.png","Farm","Projeto em desenvolvimento, com o objetivo de criar um  aplicativo em react para compartilhar de receitas","fa-youtube-play","https://youtu.be/8BvZLbKZq7c",null,null,[react,html,css,javascript])
         const proj5 = new proj("./imagens/projetos/multiplica.png","Multiplica","Aplicativo de finanças desenvolvido em JAVA, XML, SQL Lite no Android Studio","fa-youtube-play","https://www.youtube.com/watch?v=cGCnGwEJFhk",null,null,[java,xml,sqlite])
-        const proj6 = new proj("./imagens/projetos/blog.png","Blog"," Estou desenvolvendo uma aplicação em NODE.js, utilizando: Express, Mongodb, Mongoose. Aplicação em seus primeiros passos!","fa-youtube-play","https://www.youtube.com/watch?v=62UA_ZU5U1c&feature=youtu.be", "Video1 = cancel\n\nVideo2 = ok","https://youtu.be/kEd-k4kr5PU",[node,mongo])
+        const proj6 = new proj("./imagens/projetos/bigode.png","Api (Projeto Farm)","Api desenvolvida utilizando node e sql essa api tem a capacidade de registrar novas receitas, e novos usuarios, tendo todos os comando basicos de uma api","fa-youtube-play","https://www.youtube.com/watch?v=Rbnt9zzvNEU",null,null,[node,sql])
         const proj7 = new proj ("./imagens/projetos/js.png","Projetos JavaScript","Algums projetos para visualizar meus conhecimentos em javascript esses projetos foram construido de maneira basicas","","https://eversonhenr1.github.io/projetosJS/Projetos%20JavaScript/",null,null,[html,css,javascript])
         //Criar Array com todos os projetos
         let arrProj = [proj1,proj2,proj3,proj4,proj5,proj6,proj7];
@@ -206,7 +246,7 @@
 
         validation.forEach((inputs)=>{
     
-            inputs.addEventListener("input",(input)=>{
+            inputs.addEventListener("input",function google (input){
                 const label =inputs.parentNode.querySelector("label");
                 if(!label.classList.contains("labelGoogle")){
                     label.classList.add("labelGoogle")
@@ -226,10 +266,10 @@
                     
                 }
                
-            })
-            
+            }) 
             
         })
+
     
 
       
